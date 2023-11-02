4aa104ed1a5f8be131a963dd39554036ea613f76
DMITRY CHETVERIKOV <76950379+ChetverikovDV@users.noreply.github.com>
2023-11-02 20:34:24 +0400

    развлекаюсь с окошками. ну прикалывает

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,18 +6,6 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Мое первое окно");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-
-        JButton button = new JButton("Нажми меня!");
-        button.addActionListener(e -&gt; JOptionPane.showMessageDialog(frame, "Кнопка нажата!"));
-        frame.add(button, BorderLayout.SOUTH);
-
-        JLabel label = new JLabel("Привет, мир!");
-        frame.add(label, BorderLayout.NORTH);
-
-        frame.setVisible(true);
 
     }
 }
